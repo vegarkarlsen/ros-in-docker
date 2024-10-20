5eeaa45269fa0cdfb0700027a329ced4f8cd48f4
vegar <vegar1201@live.no>
2024-10-20 20:29:02 +0200

    Add zsh extra.

---
+++ extra/zsh/zsh-init_rid.zsh
@@ -0,0 +1,30 @@
+
+# Do nothing if $ROS_DISTRO is not set.
+if [[ -z $ROS_DISTRO ]]; then
+    return
+fi
+
+# Set Working Dir to home if not set
+if [[ ! -z $ROS_DOCKER_HOME ]]; then
+    ROS_DOCKER_WORKDIR="$HOME"
+fi
+
+# Soruce Ros-distro
+source "/opt/ros/$ROS_DISTRO/setup.zsh"
+
+# Fix TERM
+export TERM=xterm-256color
+
+# Set compiler to clang
+export CC=clang
+export CXX=clang++
+
+
+# See: https://www.reddit.com/r/ROS/comments/15yr1zm/ros_c_coding_setup/
+# export CLANG_BASE="--build-base build_clang --install-base install_clang"
+# export BUILD_ARGS="--symlink-install ${CLANG_BASE} --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+# alias cb="colcon build ${BUILD_ARGS}"
+
+
+# Set Worcking directory
+cd $ROS_DOCKER_WORKDIR
