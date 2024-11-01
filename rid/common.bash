28f0304319c2993d27b2f5cc018219dc4aa54876
vegar <vegar1201@live.no>
2024-11-01 10:21:51 +0100

    Fix common.

--- rid/common.bash
+++ rid/common.bash
@@ -1,18 +1,18 @@
 
 SCRIPT_PATH=$(realpath "$0")
 PROJECT_ROOT=$(dirname "$(dirname "$SCRIPT_PATH")")
-VERSION_FILE=$PROJECT_ROOT/.ros-version
+VERSION_FILE=$PROJECT_ROOT/.last-ros-version
 
 
 function set_container_name(){
     if [ -z $ROS_DOCKER_DISTRO ]; then
 
         if [ -f $VERSION_FILE ]; then
-            ROS_DOCKER_DISTRO=$(cat ${PROJECT_ROOT}/.last-ros-version)
+            ROS_DOCKER_DISTRO=$(cat ${VERSION_FILE})
         else
             echo "Error: ROS_DOCKER_DISTRO is not set. Please set it and try again.";
             exit 1;
         fi 
     fi
-    DOCKER_CONTAINER_NAME="ros-$(cat ${PROJECT_ROOT}/.ros-version)-container"
+    DOCKER_CONTAINER_NAME="ros-${ROS_DOCKER_DISTRO}-container"
 }
